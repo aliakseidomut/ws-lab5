9f292c483fdeaf0ecc8f090918a9abafd077bbb4
aliakseidomut <alexeydomut@gmail.com>
2025-11-22 11:55:49 +0300

    yui

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.post("/api/logout", authController.logout);
 app.get("/api/profile", verifyAccessToken, authController.getProfile);
 
 app.get("/", (req, res) => {
-  res.send("Hello");
+  res.send("Helloшщршщршо");
 });
 
 const startServer = async () => {
